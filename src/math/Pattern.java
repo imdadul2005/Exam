2bca50a7078445a8169fc81716fcd0f5d5b71f18
imdadul2005 <hoq.imdadul@gmail.com>
2014-07-26 14:08:36 -0400

    Exam Selenium

--- src/math/Pattern.java
+++ src/math/Pattern.java
@@ -1,26 +1,33 @@
 package math;
 
+import java.util.Scanner;
+
 public class Pattern {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		/* Read this numbers, find the pattern then implement the logic from this pattern.which will give you this output.
 		 * 100,99,98,97,96,95,94,93,92,91,90,88,86,84,82,80,78,76,74,72,70,67,64,61,58,55,52,49,46,43,40,36,32
 		 * 
 		 */
-		
-		int start = 100;
-		int decrement = 1;
-		int numberCount = 10;
-		
-		while ((start>=0)){
-			System.out.print(start + ",");
-			start = start - decrement;
-			numberCount--;
-			if(numberCount==0){
-				decrement++;
-				numberCount = 10;
-			}	
-		}
-	}
+
+
+        Scanner input  = new Scanner(System.in);
+        System.out.println("Type your starting number :\t");
+
+        int start = input.nextInt();
+        int decrement = 1;
+        int numberCount = 10;
+
+        while ((start>=0)){
+            System.out.print(start + ",");
+            start = start - decrement;
+            numberCount--;
+            if(numberCount==0){
+                decrement++;
+                numberCount = 10;
+                System.out.println();
+            }
+        }
+    }
 
 }
